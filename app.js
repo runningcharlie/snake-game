2ac66ec072ef9fa9c981c3a0818da32654cacd9d
charlie <ljwcharlie@gmail.com>
2023-03-09 10:42:21 +0800

    画出蛇的基本形状

--- app.js
+++ app.js
@@ -3,7 +3,37 @@ const ctx = canvas.getContext("2d");
 // getContext() 返回一个canvas 的 drawing context
 // drawing context可以用来在canvas内画图
 
-// 计算出游戏区域每一格的宽高
 const unit = 20;
 const row = canvas.height / unit; // 320 / 20 = 16
 const column = canvas.width / unit; // 320 / 20 = 16
+
+// 使用一个对象数组储存蛇身体的坐标
+let snake = [];
+
+snake[0] = {
+  x: 80,
+  y: 0,
+};
+
+snake[1] = {
+  x: 60,
+  y: 0,
+};
+
+snake[2] = {
+  x: 40,
+  y: 0,
+};
+
+snake[3] = {
+  x: 20,
+  y: 0,
+};
+
+// 使用数据和画板（drawing context）画出蛇的形状
+for (let i = 0; i < snake.length; i++) {
+  // 填充颜色
+  ctx.fillStyle = "lightblue";
+  // 画一个矩形，参数为 x y width height
+  ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+}
